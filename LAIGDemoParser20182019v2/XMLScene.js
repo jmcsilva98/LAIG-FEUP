780a1073a0de7ea65ec3842b9479a069f454fa8a
Ana Rita Norinho <ritanorinho98@hotmail.com>
2018-10-15 20:44:11 +0100

    tudo o que tem cubos esta direito. falta cilindro

---
+++ LAIGDemoParser20182019v2/XMLScene.js
@@ -0,0 +1,198 @@
+var DEGREE_TO_RAD = Math.PI / 180;
+
+/**
+ * XMLscene class, representing the scene that is to be rendered.
+ */
+class XMLscene extends CGFscene {
+    /**
+     * @constructor
+     * @param {MyInterface} myinterface
+     */
+    constructor(myinterface) {
+        super();
+
+        this.interface = myinterface;
+        this.lightValues = {};
+        this.index = "view1";
+        this.oldIndex = this.index;
+    }
+
+    /**
+     * Initializes the scene, setting some WebGL defaults, initializing the camera and the axis.
+     * @param {CGFApplication} application
+     */
+    init(application) {
+        super.init(application);
+
+        this.sceneInited = false;
+
+        this.initCameras();
+
+        this.enableTextures(true);
+
+        this.gl.clearDepth(100.0);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.enable(this.gl.CULL_FACE);
+        this.gl.depthFunc(this.gl.LEQUAL);
+
+        this.axis = new CGFaxis(this);
+
+    }
+
+    /**
+     * Initializes the scene cameras.
+     */
+    initCameras() {
+        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
+
+
+
+    }
+    /**
+     * Initializes the scene lights with the values read from the XML file.
+     */
+    initLights() {
+        var i = 0;
+        // Lights index.
+
+        // Reads the lights from the scene graph.
+        for (var key in this.graph.lights) {
+            if (i >= 8)
+                break;              // Only eight lights allowed by WebGL.
+
+            if (this.graph.lights.hasOwnProperty(key)) {
+                var light = this.graph.lights[key];
+
+                //lights are predefined in cgfscene
+                if (light[0]=="omni"){
+                this.lights[i].setPosition(light[2][0], light[2][1], light[2][2], light[2][3]);
+                this.lights[i].setAmbient(light[3][0], light[3][1], light[3][2], light[3][3]);
+                this.lights[i].setDiffuse(light[4][0], light[4][1], light[4][2], light[4][3]);
+                this.lights[i].setSpecular(light[5][0], light[5][1], light[5][2], light[5][3]);
+                }
+                else if (light[0]=="spot"){
+                    this.lights[i].setSpotCutOff(light[2]);
+                    this.lights[i].setSpotExponent(light[3]);
+                    this.lights[i].setPosition(light[4][0], light[4][1], light[4][2], light[4][3]);
+                    this.lights[i].setSpotDirection(light[5][0],light[5][1],light[5][2]);
+                    this.lights[i].setAmbient(light[6][0], light[6][1], light[6][2], light[6][3]);
+                    this.lights[i].setDiffuse(light[4][0], light[4][1], light[4][2], light[7][3]);
+                    this.lights[i].setSpecular(light[8][0], light[8][1], light[8][2], light[8][3]);
+                }
+
+                this.lights[i].setVisible(true);
+
+                i++;
+            }
+        }
+    }
+
+
+    /* Handler called when the graph is finally loaded.
+     * As loading is asynchronous, this may be called already after the application has started the run loop
+     */
+    onGraphLoaded() {
+       // this.camera.near = this.graph.near;
+      //  this.camera.far = this.graph.far;
+
+        //TODO: Change reference length according to parsed graph
+        this.axis = new CGFaxis(this,this.graph.referenceLength);
+
+        // TODO: Change ambient and background details according to parsed graph
+        this.setGlobalAmbientLight(this.graph.ambientIlumination[0],this.graph.ambientIlumination[1],
+                                    this.graph.ambientIlumination[2],this.graph.ambientIlumination[3]);
+        this.gl.clearColor(this.graph.backgroundIlumination[0],this.graph.backgroundIlumination[1],
+            this.graph.backgroundIlumination[2],this.graph.backgroundIlumination[3]);
+
+
+        this.initLights();
+
+        // Adds lights group.
+        this.interface.addLightsGroup(this.graph.lights);
+
+        //Adds views group.
+        this.interface.addViewsGroup(this.graph.newView);
+
+        this.sceneInited = true;
+    }
+
+
+    /**
+     * Displays the scene.
+     */
+    display() {
+        // ---- BEGIN Background, camera and axis setup
+
+        // Clear image and depth buffer everytime we update the scene
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+        //if(this.oldIndex != this.index){
+
+
+          /*if(this.graph.perspectiveComponent[this.index] != null){
+          this.camera.fov = this.graph.perspectiveComponent[this.index][0];
+          this.camera.near = this.graph.perspectiveComponent[this.index][1];
+          this.camera.far = this.graph.perspectiveComponent[this.index][2];
+          this.camera.setPosition(this.graph.perspectiveComponent[this.index][3]);
+          this.camera.setTarget(this.graph.perspectiveComponent[this.index][4]);
+        }else{
+          this.camera.left = this.graph.orthoComponent[this.index][0];
+          this.camera.right = this.graph.orthoComponent[this.index][1];
+          this.camera.bottom = this.graph.orthoComponent[this.index][2];
+          this.camera.top = this.graph.orthoComponent[this.index][3];
+          this.camera.near = this.graph.orthoComponent[this.index][4];
+          this.camera.far = this.graph.orthoComponent[this.index][5];
+          this.camera.setPosition(this.graph.orthoComponent[this.index][6]);
+          this.camera.setTarget(this.graph.orthoComponent[this.index][7]);
+          this.camera.setUp(this.graph.orthoComponent[this.index][8]);
+        }*/
+      /*  this.interface.setActiveCamera(this.graph.newView[this.index]);
+        this.oldIndex = this.index;
+      }*/
+
+
+
+
+        // Initialize Model-View matrix as identity (no transformation
+        this.updateProjectionMatrix();
+        this.loadIdentity();
+
+        // Apply transformations corresponding to the camera position relative to the origin
+        this.applyViewMatrix();
+
+        this.pushMatrix();
+
+        if (this.sceneInited) {
+            // Draw axis
+            this.axis.display();
+
+            var i = 0;
+            for (var key in this.lightValues) {
+                if (this.lightValues.hasOwnProperty(key)) {
+                    if (this.lightValues[key]) {
+                        this.lights[i].setVisible(true);
+                        this.lights[i].enable();
+                    }
+                    else {
+                        this.lights[i].setVisible(false);
+                        this.lights[i].disable();
+                    }
+                    this.lights[i].update();
+                    i++;
+                }
+            }
+
+
+            // Displays the scene (MySceneGraph function).
+            this.graph.displayScene();
+        }
+        else {
+            // Draw axis
+            this.axis.display();
+        }
+
+        this.popMatrix();
+        // ---- END Background, camera and axis setup
+    }
+}
