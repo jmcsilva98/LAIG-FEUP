bc497cc228b92f1440013fa3ed4874cfe48cb8f9
Joana Silva <joana.m.c.silva.pt@gmail.com>
2018-10-31 17:39:35 +0000

    added things

--- TP2/animation/CircularAnimation.js
+++ TP2/animation/CircularAnimation.js
@@ -10,14 +10,35 @@ class CircularAnimation extends Animation{
   super(scene, animationId, animationTime);
   this.center = center;
   this.radius = radius;
-  this.initialAngle = initialAngle;
-  this.rotAngle = rotAngle;
-
-
+  this.initialAngle = initialAngle * Math.PI/180;
+  this.rotAngle = rotAngle* Math.PI/180;
 
   }
 
+  calculateAnimation(currentTime){
 
+  var identMatrix = mat4.create();
+  if(this.currentTime < 0)
+  {
+    this.currentTime = currentTime;
+    return identMatrix;
+  }
+
+  //calculates the time passed in seconds (time given in milliseconds)
+  var deltaTime = (currentTime - this.currentTime)/1000;
+
+  if(this.animationTime < deltaTime)
+    deltaTime = this.animationTime;
+
+  var deltaAngle = this.initialAngle + this.rotAngle / this.animationTime * deltaTime;
+
+  mat4.translate(identMatrix,identMatrix, this.center);
+  mat4.rotate(identMatrix,identMatrix,deltaAngle,[0,1,0]);
+  mat4.translate(identMatrix,identMatrix, [this.radius,0,0]);
+
+
+  return identMatrix;
+  };
 
 
 };
--- TP2/animation/LinearAnimation.js
+++ TP2/animation/LinearAnimation.js
@@ -9,13 +9,46 @@ class LinearAnimation extends Animation{
 
   super(scene, animationId, animationTime);
   this.controlPoints = controlPoints;
+  this.totalDistance;
+  this.movementDistance = [];
 
+  this.calculateDistance();
 
+  this.velocity = this.totalDistance / this.animationTime;
 
 
   }
 
-  
+  calculateDistance(){
+
+    for(var i = 0; i < controlPoints.length - 1; i++){
+
+      var distance = Math.sqrt(Math.pow(this.controlPoints[i+1][0] - this.controlPoints[i][0],2) + Math.pow(this.controlPoints[i+1][1] - this.controlPoints[i][1],2) + Math.pow(this.controlPoints[i+1][2] - this.controlPoints[i][2],2));
+      this.movementDistance.push(distance);
+      this.totalDistance += distance;
+
+    }
+
+  };
+
+  calculateAngle(initialPoint, finalPoint){
+
+    var angle = Math.atan2(finalPoint[2] - initialPoint[2], finalPoint[0] - initialPoint[0]);
+    if(finalPoint[2]-initialPoint[2] < 0){
+      angle += Math.PI;
+    }
+
+
+    return angle;
+  };
+
+  update(){
+
+
+
+
+
+  };
 
 
 
